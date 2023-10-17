5e18b231cdaa8078a40251c5d9b8ce242fa97012
suporte18 <suporte18@ORTEP-27>
2023-10-17 12:30:18 -0300

    Adicionar arquivos de projeto.

---
+++ ProgramaTest1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+class Variaveis
+{
+    static void Main()
+    {
+        // Nesse programa foi feito o calculo da area de um retangulo.
+
+        //int x = 4;
+        // double y = 3.3;
+        //const double frequence = 60;
+
+        //double area = x * y;
+
+        //Console.WriteLine("A área do quadrado é " + area);
+
+
+        // conversão implícita 
+
+        //    int x = 4;
+        // double x = y; 
+
+        //Console.WriteLine(y);
+
+        // CONVERSÃO EXPLÍCITA 
+
+        //double nota = 8.6;
+        //int notaconvertida = (int)nota;
+        //Console.WriteLine(notaconvertida);
+
+
+        //Convert
+
+    //    Console.WriteLine("digite qual numero é sua casa: ");
+    //    String numeroCasa = Console.ReadLine();
+    //    int numero = int.Parse(numeroCasa);
+    //    Console.WriteLine("Número é sua casa: {0}", numero);
+
+    //    numero = Convert.ToInt32(numeroCasa);
+    //    Console.WriteLine(numero);
+
+
+    }
+
+} 
+
